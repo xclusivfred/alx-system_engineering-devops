cd01b1d6e41f830adb57d23bda275952b29b8e74
xclusivfred <xclusivfred@gmail.com>
2023-03-28 12:04:55 +0100

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,22 @@
-# Puppet manifest to install nginx
+# File:   7-puppet_install_nginx_web_server.pp
+# Author: Alex Orland Arévalo Tribaldos
+# email:  <3915@holbertonschool.com>
+
+# Using Puppet| Install Nginx server, setup and configuration
+
 package { 'nginx':
-  ensure => installed,
+  ensure => 'installed'
 }
 
-file_line { 'aaaaa':
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+file_line { 'redirection-301':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
 }
 
 service { 'nginx':
