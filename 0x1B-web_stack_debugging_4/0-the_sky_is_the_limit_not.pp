932a1f29d4f0d9fbf80e5b2215d1929f2c871c2d
xclusivfred <xclusivfred@gmail.com>
2023-05-15 06:51:44 +0100

    added 0x1B

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Fixing limited requests problem 
+exec { "sed -i 's/15/4096/' /etc/default/nginx && sudo service nginx restart":
+path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
+
+
+
+
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,4 @@
+# Fixing user limit
+exec { "sed -i -E '56s/[0-9]+/5000/;57s/[0-9]+/5000/' /etc/security/limits.conf":
+  path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
